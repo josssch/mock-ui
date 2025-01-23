7f8624def7335c9b64a08126d9c0bf0da1f43595
Josh <hi@jossh.dev>
2025-01-23 15:02:01 -0500

    feat: `MockInputMultiline` now consistent across browsers

--- src/lib/MockInputMultiline.svelte
+++ src/lib/MockInputMultiline.svelte
@@ -2,12 +2,17 @@
     lang="ts"
     module
 >
-    import type { HTMLTextareaAttributes } from 'svelte/elements'
+    import type {
+        FormEventHandler,
+        HTMLTextareaAttributes,
+    } from 'svelte/elements'
 
     export interface MockInputMultilineProps extends HTMLTextareaAttributes {}
 </script>
 
 <script lang="ts">
+    import { browser } from '$app/environment'
+
     import {
         COMPONENT_BORDER_FOCUS,
         COMPONENT_DISABLED,
@@ -17,14 +22,36 @@
     let {
         value = $bindable(''),
         class: clazz,
+        oninput,
         ...props
     }: MockInputMultilineProps = $props()
+
+    const fieldResizer:
+        | FormEventHandler<HTMLTextAreaElement>
+        | null
+        | undefined =
+        !browser || CSS.supports('field-sizing', 'content')
+            ? oninput
+            : event => {
+                  oninput?.(event)
+                  if (event.defaultPrevented) return
+
+                  const target = event.target
+                  if (!(target instanceof HTMLTextAreaElement)) return
+
+                  target.style.height = ''
+                  target.style.height = `${target.scrollHeight}px`
+
+                  target.style.width = ''
+                  target.style.width = `${target.scrollWidth}px`
+              }
 </script>
 
 <textarea
     {...props}
+    oninput={fieldResizer}
     class={cn(
-        'rounded-default bg-subtle py-sm px-md field-sizing-content resize-none transition-colors',
+        'rounded-default bg-subtle py-sm px-md field-sizing-content max-h-32 min-h-16 w-64 resize-none transition-colors',
         COMPONENT_BORDER_FOCUS,
         COMPONENT_DISABLED,
         clazz,
