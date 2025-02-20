9b5c038a007c2bb093f1e3aa07fb9c011953d0aa
Josh <hi@jossh.dev>
2025-02-20 01:01:34 -0500

    fix: remove unnecessary styling

--- src/lib/ui/MockInput.svelte
+++ src/lib/ui/MockInput.svelte
@@ -34,10 +34,7 @@
     const sideStyles = 'absolute top-1/2 size-6 -translate-y-1/2'
 </script>
 
-<div
-    class="group/input relative
-    {ghost ? '' : '[--color-background:_var(--color-subtle)]'}"
->
+<div class="group/input relative">
     {#if left}
         <span class="{sideStyles} {ghost ? 'left-1' : 'left-1.5'}">
             {@render left()}
