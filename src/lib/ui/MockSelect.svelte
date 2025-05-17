618dcb0938bb76015f2548286a03fe015dbe6afd
Josh <hi@jossh.dev>
2025-05-17 13:09:34 -0400

    style(select): fix glossy appearance on Safari

--- src/lib/ui/MockSelect.svelte
+++ src/lib/ui/MockSelect.svelte
@@ -22,19 +22,38 @@
     }: MockSelectProps = $props()
 </script>
 
-<select
-    {...props}
-    class={cn(
-        'group/select rounded-default bg-container-dim px-md py-sm transition-colors',
-        COMPONENT_BORDER_HOCUS,
-        COMPONENT_DISABLED,
-        clazz,
-    )}
-    bind:value
->
-    {#if children}
-        {@render children()}
-    {:else}
-        <option value="null">No Options</option>
-    {/if}
-</select>
+<div class="relative inline-block">
+    <select
+        {...props}
+        class={cn(
+            'group/select appearance-none rounded-default bg-container-dim py-sm pr-xl pl-md transition-colors',
+            COMPONENT_BORDER_HOCUS,
+            COMPONENT_DISABLED,
+            clazz,
+        )}
+        bind:value
+    >
+        {#if children}
+            {@render children()}
+        {:else}
+            <option value="null">No Options</option>
+        {/if}
+    </select>
+
+    <!-- HeroIcon's chevron-down -->
+    <svg
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="pointer-events-none absolute top-1/2 right-md size-4 -translate-y-1/2"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+        />
+    </svg>
+</div>
