5974163f88e42b8e25f3a17c5c514c8e81b990c3
Josh <hi@jossh.dev>
2025-08-21 14:56:05 -0400

    feat: add very basic SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,42 @@
         href="https://rsms.me/inter/inter.css"
         referrerpolicy="no-referrer"
     />
+
+    <!-- Favicons for 48x48, 32x32, 16x16 -->
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href="/favicon-48x48.png"
+    />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+    />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+    />
+
+    <!-- Very very simplistic SEO -->
+    <meta
+        name="description"
+        content="Demo site for the Mock UI component library."
+    />
+    <meta
+        name="author"
+        content="Josh <contact@jossh.dev>"
+    />
+    <meta
+        name="robots"
+        content="index, follow"
+    />
+
+    <title>Mock UI Demo &bullet; Josh</title>
 </svelte:head>
 
 {@render children()}
